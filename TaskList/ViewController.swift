1606ad0d81a96b286565e53874364face5b1a23e
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-05-26 15:00:47 +0300

    Initial Commit

---
+++ TaskList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskList
+//
+//  Created by MikhaiL on 26.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
