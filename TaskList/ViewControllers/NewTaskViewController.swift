ce6e699511d02ca7ee5dd4975377a16646cc21c0
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-05-29 12:11:10 +0300

    adding pattern simple factory  to create buttons

--- TaskList/ViewControllers/NewTaskViewController.swift
+++ TaskList/ViewControllers/NewTaskViewController.swift
@@ -7,6 +7,58 @@
 
 import UIKit
 
+/// Protocol that defines an interface for creating buttons.
+protocol ButtonFactory {
+    /// Creates a button.
+    ///
+    /// - Returns: A `UIButton` instance.
+    func createButton() -> UIButton
+}
+
+/// Custom implementation of the `ButtonFactory` protocol.
+final class FilledButtonFactory: ButtonFactory {
+    /// The title of the button.
+    let title: String
+    
+    /// The background color of the button.
+    let color: UIColor
+    
+    /// The action to be performed when the button is tapped.
+    let action: UIAction
+    
+    /// Initializes a new custom button factory with the provided title, color, and action.
+    ///
+    /// - Parameters:
+    ///   - title: The title of the button.
+    ///   - color: The background color of the button.
+    ///   - action: The action to be performed when the button is tapped.
+    init(title: String, color: UIColor, action: UIAction) {
+        self.title = title
+        self.color = color
+        self.action = action
+    }
+    
+    /// Creates a button with the predefined title, color and action.
+    ///
+    /// - Returns: A `UIButton` instance with the predefined attributes.
+    func createButton() -> UIButton {
+        // Create an attribute container and set the font
+        var attributes = AttributeContainer()
+        attributes.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        // Create a filled button configuration and set the background color and title
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.baseBackgroundColor = color
+        buttonConfiguration.attributedTitle = AttributedString(title, attributes: attributes)
+        
+        // Create a button using the configuration and action, and disable autoresizing
+        let button = UIButton(configuration: buttonConfiguration, primaryAction: action)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }
+}
+
 final class NewTaskViewController: UIViewController {
     private lazy var taskTextField: UITextField = {
         let textField = UITextField()
@@ -17,37 +69,25 @@ final class NewTaskViewController: UIViewController {
     }()
     
     private lazy var saveButton: UIButton = {
-        // Set attributes for button title
-        var attributes = AttributeContainer()
-        attributes.font = UIFont.boldSystemFont(ofSize: 18)
-        
-        // Create button
-        var buttonConfiguration = UIButton.Configuration.filled()
-        buttonConfiguration.baseBackgroundColor = UIColor(named: "MilkBlue")
-        buttonConfiguration.attributedTitle = AttributedString("Save Task", attributes: attributes)
-        
-        let button = UIButton(configuration: buttonConfiguration, primaryAction: UIAction { [unowned self] _ in
-            save()
-        })
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
+        let filledButtonFactory = FilledButtonFactory(
+            title: "Save Task",
+            color: UIColor(named: "MilkBlue") ?? .systemBlue,
+            action: UIAction { [unowned self] _ in
+                save()
+            }
+        )
+        return filledButtonFactory.createButton()
     }()
     
     private lazy var cancelButton: UIButton = {
-        // Set attributes for button title
-        var attributes = AttributeContainer()
-        attributes.font = UIFont.boldSystemFont(ofSize: 18)
-        
-        // Create button
-        var buttonConfiguration = UIButton.Configuration.filled()
-        buttonConfiguration.baseBackgroundColor = UIColor(named: "MilkRed")
-        buttonConfiguration.attributedTitle = AttributedString("Cancel", attributes: attributes)
-        
-        let button = UIButton(configuration: buttonConfiguration, primaryAction: UIAction { [unowned self] _ in
-            dismiss(animated: true)
-        })
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
+        let filledButtonFactory = FilledButtonFactory(
+            title: "Cancel",
+            color: UIColor(named: "MilkRed") ?? .systemRed,
+            action: UIAction { [unowned self] _ in
+                dismiss(animated: true)
+            }
+        )
+        return filledButtonFactory.createButton()
     }()
     
     override func viewDidLoad() {
